69569d74c73b847dffb7488483ea570761748cb1
hkhanh99 <nguyenhuuhoangkhanh1999@gmail.com>
2025-03-29 20:08:05 +0700

    Create Product Details

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Product/GenderCollection";
 import NewArrivals from "../components/Product/NewArrivals";
+import ProductDetails from "../components/Product/ProducDetails";
 
 const Home =() => {
     return <div>
         <Hero/>
         <GenderCollection/>
         <NewArrivals/>
+        <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
+        <ProductDetails/>
     </div>;
 }
 export default Home;

bb099b3731b9600908bbd169f328ddd3494764d4
hkhanh99 <nguyenhuuhoangkhanh1999@gmail.com>
2025-03-30 11:36:51 +0700

    Done HomePage

--- frontend/src/components/Product/ProducDetails.jsx
+++ frontend/src/components/Product/ProducDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import ProductGrid from "./ProductGrid.jsx";
 
 const selectedProduct = {
     name: "Nintendo Switch OLED",
@@ -20,6 +21,33 @@ const selectedProduct = {
         }
     ]
 }
+const similarProducts = [
+    {
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        images: [{url:"https://picsum.photos/500/500?random=3"}]
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        images: [{url:"https://picsum.photos/500/500?random=4"}]
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        images: [{url:"https://picsum.photos/500/500?random=5"}]
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        images: [{url:"https://picsum.photos/500/500?random=6"}]
+    }
+]
+
 const ProductDetails = () => {
     const [mainImage, setMainImage] = useState("");
     const [selectedColor, setSelectedColor] = useState("");
@@ -157,6 +185,12 @@ const ProductDetails = () => {
                         </table>
                     </div>
                 </div>
+            </div>
+            <div className="mt-20">
+                <h2 className ="text-2xl text-center font-medium mb-4">
+                    You May Also Like
+                </h2>
+                <ProductGrid products={similarProducts}/>
             </div>
         </div>
     </div>

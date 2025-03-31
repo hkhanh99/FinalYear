75d7ea693213f3af1dffd9f3d814dee3649560e8
hkhanh99 <nguyenhuuhoangkhanh1999@gmail.com>
2025-03-31 15:37:57 +0700

    Create Checkout UI, Paypal Payment

---
+++ frontend/src/components/Cart/PaypalButton.jsx
@@ -0,0 +1,50 @@
+import {PayPalButtons, PayPalScriptProvider} from "@paypal/react-paypal-js"
+
+
+const PaypalButton = ({amount, onSuccess, onError}) => {
+    return(<PayPalScriptProvider options={{"client-id":
+    "AW1N3uoywsQpSkEF2cDqDwWl7lcEoAlslMAcD-KjLRyOf3wMekLvzKRMHZTVjGnDSHTWMH3DRCehdWh-"}}>
+
+        <PayPalButtons 
+        style= {{layout: "vertical"}}
+        createOrder={(data, actions) => {
+            return actions.order.create ({
+                purchase_units: [{amount: {value: amount}}]
+            })
+        }}
+        onApprove = {(data,actions) => {
+            return actions.order.capture().then(onSuccess)
+        }}
+        onError={onError}
+        />
+    </PayPalScriptProvider>)
+}
+export default PaypalButton
+// import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
+
+// const PaypalButton = ({ amount, onSuccess, onError }) => {
+//     return (
+//         <PayPalScriptProvider options={{
+//             "client-id": "AW1N3uoywsQpSkEF2cDqDwWl7lcEoAlslMAcD-KjLRyOf3wMekLvzKRMHZTVjGnDSHTWMH3DRCehdWh-"
+//         }}>
+//             <PayPalButtons 
+//                 style={{ layout: "vertical" }}
+//                 createOrder={(data, actions) => {
+//                     return actions.order.create({
+//                         purchase_units: [{ amount: { value: amount } }]
+//                     })
+//                 }}
+//                 onApprove={(data, actions) => {
+//                     return actions.order.capture().then((details) => {
+//                         if (onSuccess) onSuccess(details);
+//                     });
+//                 }}
+//                 onError={(err) => {
+//                     if (onError) onError(err);
+//                 }}
+//             />
+//         </PayPalScriptProvider>
+//     )
+// }
+
+// export default PaypalButton

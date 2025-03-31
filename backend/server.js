e026fc1aac4060a624344c08b2a60dddd73dfe22
hkhanh99 <nguyenhuuhoangkhanh1999@gmail.com>
2025-03-31 23:05:07 +0700

    API login register

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
-
+const userRoutes = require("./routes/userRoutes")
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -18,6 +18,9 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("WELCOME TO GAMESTORE API!");
 });
+
+//API route
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

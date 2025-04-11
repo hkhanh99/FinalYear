0c53c5318b053a1459783a50d8a74fd06ac78b77
hkhanh99 <nguyenhuuhoangkhanh1999@gmail.com>
2025-04-11 20:23:53 +0700

    AdminRoute, Subcribesroute, uploadRoute

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,14 @@ const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes")
 const productRoutes = require("./routes/productRoutes")
 const cartRoutes = require("./routes/cartRoutes")
-const checkoutRoutes = require("./routes/CheckoutRoutes")
+const checkoutRoutes = require("./routes/checkoutRoutes")
 const orderRoutes = require("./routes/orderRoutes")
+const uploadRoutes = require("./routes/uploadRoutes")
+const subscribeRoutes = require("./routes/subscribeRoute")
+const adminRoutes = require("./routes/adminRoutes")
+const productAdminRoutes = require("./routes/productAdminRoutes")
+const adminOrderRoutes = require("./routes/adminOrderRoutes")
+
 
 const app = express();
 app.use(express.json());
@@ -29,6 +35,13 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api", subscribeRoutes);
+
+//admin route
+app.use("/api/admin/users", adminRoutes)
+app.use("/api/admin/products", productAdminRoutes)
+app.use("/api/admin/orders", adminOrderRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
